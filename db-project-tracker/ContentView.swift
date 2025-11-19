a9e0f8a23da2d0ff82d869e9a7522bb89070b747
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-19 19:21:15 +0100

    Initial Commit

---
+++ db-project-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  db-project-tracker
+//
+//  Created by Brannon Garrett on 19.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Brannon Garrett on 19.11.25.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
